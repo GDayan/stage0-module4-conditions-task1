954d50fc4deddffbae4fb2331bab154dd69e48c5
GDayan <daykanot@gmail.com>
2024-03-26 23:18:02 +0300

    done conditions task 1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (false) {
+            // Do nothing if condition is false
+        } else {
+            System.out.println("false");
+        }
     }
 }
+
